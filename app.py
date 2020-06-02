5de0766a6bdad83911989d770cecf6b5fc892034
harshitmittal99 <144harshit@gmail.com>
2020-06-02 16:04:01 +0530

    boycott_china basic

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask,render_template,request,redirect,url_for,session
+from flask_mysqldb import MySQL
+import MySQLdb
+import sys
+app=Flask(__name__)
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "Harshit"
+app.config['MYSQL_PASSWORD'] = "Harshit"
+app.config['MYSQL_DB'] = "boycott_china"
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from chinese_apps")
+    c_apps = cur.fetchall()
+    return render_template('home.html',result = c_apps)
+
+
+@app.route('/alternatives')
+def alternative():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from alternatives")
+    c_apps = cur.fetchall()
+    return render_template('alternative.html',result = c_apps)
+
+
+@app.route('/Search',methods=["POST"])
+def Search():
+    search = request.form['search']
+    cur = mysql.connection.cursor()
+    cur.execute("select * from alternatives where LOWER(chinese_app) LIKE '%"+search.lower()+"%'")
+    c_apps = cur.fetchall()
+    return render_template('alternative.html',result = c_apps)
+
+@app.route('/suggest')
+def suggest():
+    return render_template('suggest.html')
+
+@app.route('/getBig',methods = ['POST'])
+def getBig():
+    Suggestion = {}
+    appName = request.form['AppName']
+    alternate = request.form['alternate']
+    Link = request.form['Link']
+    Suggestion['appName'] = appName
+    Suggestion['alternate'] = alternate
+    Suggestion['Link'] = Link
+    print(Suggestion)
+    sys.stdout.flush()
+    return render_template('thanks.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
